2dac2390d69edc80cedca8a90e0d33458f45c147
Will Farley <wfarley@amazon.com>
2020-06-13 01:50:18 -0700

    Fix exports test

--- src/index.spec.js
+++ src/index.spec.js
@@ -2,9 +2,9 @@ const WorkspaceResolver = require('./index')
 
 describe('WorkspaceResolver', () => {
   it('exports prepare-workspace-alias', () => {
-    expect(WorkspaceResolver.aliases).toBeTruthy()
+    expect(WorkspaceResolver.resolve).toBeTruthy()
   })
   it('exports spawn-watch-process', () => {
-    expect(WorkspaceResolver.spawnWatchProcess).toBeTruthy()
+    expect(WorkspaceResolver.spawn).toBeTruthy()
   })
 })
